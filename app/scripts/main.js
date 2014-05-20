d46282d077a813f542b523b52fbad5ef254f56c6
Jeanie Chung <jeanie.chung@gmail.com>
2014-05-20 13:57:18 -0500

    fixed line scaling to graph

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -53,63 +53,66 @@ var data = [
     description: "passive, positive"
   }];
 
-function getDate(d) {
-    return new Date(d.date);
-}
+  function getDate(d) {
+      return new Date(d.date);
+  }
 
-var minDate = getDate(data[0]),
-    maxDate = getDate(data[data.length-1]);
-var x = d3.time.scale()
-    .domain([minDate, maxDate]).range([0, w]);
-x.tickFormat(d3.time.format("%m-%d"));
-var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
-
-
-var line = d3.svg.line()
- .x(function(d) {
-	return x(new Date(d.date)); 
- })
- .y(function(d) {
-   return y(d.x);
- });
+  var minDate = getDate(data[0]),
+      maxDate = getDate(data[data.length-1]);
+  var x = d3.time.scale()
+      .domain([minDate, maxDate]).range([0, w]);
+  x.tickFormat(d3.time.format("%m-%d"));
+  var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
 
 
-// Add an SVG element with the desired dimensions and margin.
-var graph = d3.select("#chart").append("svg:svg")
-.attr("width", w + m[1] + m[3])
-.attr("height", h + m[0] + m[2])
-.append("svg:g")
-.attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+  var line = d3.svg.line()
+   .x(function(d) {
+  	return x(new Date(d.date)); 
+   })
+   .y(function(d) {
+     return y(d.x);
+   });
 
-// create yAxis
-var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-// Add the x-axis.
-graph.append("svg:g")
-.attr("class", "x axis")
-.attr("transform", "translate(0," + h + ")")
-.call(xAxis);
+  // Add an SVG element with the desired dimensions and margin.
+  var graph = d3.select("#chart").append("svg:svg")
+  .attr("width", w + m[1] + m[3])
+  .attr("height", h + m[0] + m[2])
+  .append("svg:g")
+  .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
-
-// create left yAxis
-var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-// Add the y-axis to the left
-graph.append("svg:g")
-.attr("class", "y axis")
-.attr("transform", "translate(-25,0)")
-.call(yAxisLeft);
-
-
-var clip = graph.append("defs").append("svg:clipPath")
-.attr("id", "clip")
-.append("svg:rect")
-.attr("id", "clip-rect")
-.attr("x", "0")
-.attr("y", "0")
-.attr("width", w)
-.attr("height", h);
+  // create yAxis
+  var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+  // Add the x-axis.
+  graph.append("svg:g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + h + ")")
+  .call(xAxis);
 
 
-var path = graph.append("svg:path").attr("class","path").attr("clip-path", "url(#clip)").attr("d", line(data));
+  // create left yAxis
+  var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+  // Add the y-axis to the left
+  graph.append("svg:g")
+  .attr("class", "y axis")
+  .attr("transform", "translate(-25,0)")
+  .call(yAxisLeft);
+
+  var clip = graph.append("defs").append("svg:clipPath")
+  .attr("id", "clip")
+  .append("svg:rect")
+  .attr("id", "clip-rect")
+  .attr("x", "0")
+  .attr("y", "0")
+  .attr("width", w)
+  .attr("height", h);
+
+
+function drawLine() {
+  graph.selectAll(".path").remove(); //remove previous line
+  var path = graph.append("svg:path").attr("class","path").attr("clip-path", "url(#clip)").attr("d", line(data)); //redraw new line
+};
+
+drawLine();
 
 $("#slider").dateRangeSlider({
     defaultValues:{
@@ -127,6 +130,6 @@ $("#slider").bind("valuesChanging", function(e, data){
   x.domain([data.values.min, data.values.max]);
   graph.transition().duration(750)
     .select(".x.axis").call(xAxis);
-    path.selectAll(".path").attr("d", line(data)); //can't get the line to redraw to scale
+  drawLine();
 });
 
